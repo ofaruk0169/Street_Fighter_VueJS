3ad3afdfca8695eb28fae761095ec67200531997
Omar Faruk <ofaruk0169@gmail.com>
2020-09-13 18:38:17 +0100

    player can throw a hadouken

--- app.js
+++ app.js
@@ -10,6 +10,37 @@ new Vue({
             this.gameIsRunning = true;
             this.ryuHealth = 100;
             this.kenHealth = 100;
+        },
+        hadouken: function() {
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+            this.kenHealth -= damage;
+
+            if (this.kenHealth <= 0) {
+                alert('You Won!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+            this.ryuHealth -= damage;
+
+            if (this.ryuHealth <= 0) {
+                alert('You Lost!');
+                this.gameIsRunning = false;
+            }
+        },
+        shoryuken: function() {
+
+        },
+        tatsumaki_senpukyaku: function() {
+
+        },
+        give_up: function() {
+
         }
     }
 });

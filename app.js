7e754531fc5155a030808772da6b35ecd57db39b
Omar Faruk <ofaruk0169@gmail.com>
2020-09-14 19:46:09 +0100

    the game is complete

--- app.js
+++ app.js
@@ -3,31 +3,65 @@ new Vue({
     data: {
         ryuHealth: 100,
         kenHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.ryuHealth = 100;
             this.kenHealth = 100;
+            this.turns = [];
         },
         hadouken: function() {
-            this.kenHealth -= this.calculateDamage(3,10);
-
+            var damage = this.calculateDamage(3,10);
+            this.kenHealth -= damage;
+            this.turns.unshift({
+                isRyu: true,
+                text: 'Ryu throws a hadouken for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
-            this.ryuHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+
+            this.kenAttacks();
         },
         shoryuken: function() {
+            var damage = this.calculateDamage(10,20)
+            this.kenHealth -= damage;
+            this.turns.unshift({
+                isRyu: true,
+                text: 'Shoryuken! ' + damage
+            });
+            if (this.checkWin()) {
+                return;
+            }
 
+            this.kenAttacks();
         },
         tatsumaki_senpukyaku: function() {
-
+            if (this.ryuHealth <= 90) {
+                this.ryuHealth += 10;
+            } else {
+                this.ryuHealth = 100;
+            }
+            this.turns.unshift({
+                isRyu: true,
+                text: 'Tatsumaki Senpukyaku! '
+            });
+            this.kenAttacks();
         },
         give_up: function() {
-
+            this.gameIsRunning = false;
+        },
+        kenAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.ryuHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isRyu: false,
+                text: 'Ken spams Shoryuken for ' + damage
+            });
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
